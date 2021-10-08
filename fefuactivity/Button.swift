fb53804192ff4d38c38762053765074ee430a7c9
Cleamaster322 <82811442+Cleamaster322@users.noreply.github.com>
2021-10-08 16:29:26 +1000

    FirstButton

---
+++ fefuactivity/Button.swift
@@ -0,0 +1,24 @@
+//
+//  Button.swift
+//  fefuactivity
+//
+//  Created by wsr4 on 08.10.2021.
+//
+
+import UIKit
+
+class ButtonType1: UIButton{
+    required init?(coder: NSCoder){
+        super.init(coder:coder)
+        
+        commonInit()
+        
+    }
+    
+    private func commonInit(){
+        self.layer.cornerRadius = 12
+        self.backgroundColor = UIColor(named: "ButtonColor")
+        
+    }
+    
+}
