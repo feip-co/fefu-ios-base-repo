150c3be7568b14744e051ea23f28bcf64a597959
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2021-10-13 13:22:39 +1000

    Revert "some fix datatable"
    This reverts commit 5e0410f37d9429aec7f0a0a228ac561e24fa1a0c.

--- fefuactivity/fefuButtonType1.swift
+++ fefuactivity/fefuButtonType1.swift
@@ -17,5 +17,7 @@ class FEFUButtonType1: UIButton{
     private func commonInit(){
         self.layer.cornerRadius = 12
         self.backgroundColor = UIColor(named: "ButtonType1Color" )
+        self.frame.size = CGSize(width: 334.0, height: 56.0);
+        
     }
 }
