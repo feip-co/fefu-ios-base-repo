9e7f71635a8e83ae55b3f5bb3b7eaa9880e9243c
Sam <skopin.san@students.dvfu.ru>
2021-10-14 14:48:59 +1000

    layouts finished

---
+++ fefuactivity/RegViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegViewController.swift
+//  fefuactivity
+//
+//  Created by wsr3 on 14.10.2021.
+//
+
+import UIKit
+
+class RegViewController: UIViewController {
+
+    @IBOutlet weak var buttonReg: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
