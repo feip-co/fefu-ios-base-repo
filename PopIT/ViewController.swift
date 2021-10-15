e88dae900eb87cd573d2313e9ef1a059a3275429
Katakinov <84549722+Katakinov@users.noreply.github.com>
2021-10-15 18:06:45 +1000

    Add files via upload

---
+++ PopIT/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PopIT
+//
+//  Created by wsr2 on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //@IBAction func regButton(_ sender: Any) {
+    //}
+    /*
+    @IBAction func registrButton(_ sender: UIButton) {
+        let newVC = storyboard?.instantiateViewController(withIdentifier: "RegistrPage")
+        navigationController?.pushViewController(newVC!, animated: true)
+    }
+    */
+}
+
