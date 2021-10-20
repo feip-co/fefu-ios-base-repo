4c40bdd7f103871ea4a97f3c8ff3886d6e212a9a
XxXBlackKamaroXxX <81573956+XxXBlackKamaroXxX@users.noreply.github.com>
2021-10-20 17:04:29 +1000

    Add files via upload
    homework 1 lecture

---
+++ HomeTask_1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  HomeTask_1
+//
+//  Created by wsr4 on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        }
+
+}
+
+
+class ViewController1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
+
+
+
