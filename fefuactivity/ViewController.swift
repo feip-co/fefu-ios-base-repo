aeaca82292a02ba66224039d9834b808aaf9ad64
Katakinov <84549722+Katakinov@users.noreply.github.com>
2021-10-29 00:27:33 +1000

    почти

--- fefuactivity/ViewController.swift
+++ fefuactivity/ViewController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var genderField: UITextField!
+    
+    let genders = ["Мужской", "Женский"]
+    
+    var pickerView = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        //genderField.inputView = pickerView
+        //genderField.textAlignment = .center
+        //genderField.placeholder = "Select gender"
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return genders.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return genders[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        genderField.text = genders[row]
+        genderField.resignFirstResponder()
     }
 }
 
