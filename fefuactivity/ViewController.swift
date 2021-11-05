0e1dc36c43c2ce1f01490b7531c0a21a7a19f3d7
Zahar <zahark13@gmail.com>
2021-11-05 20:18:49 +1000

    Table Test

--- fefuactivity/ViewController.swift
+++ fefuactivity/ViewController.swift
@@ -8,27 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var MyTable: UITableView!
-    let mytext = ["вчера", "22 октября"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        MyTable.delegate = self
-        MyTable.dataSource = self
     }
 
-    
-    
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mytext.count
-}
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = MyTable.dequeueReusableCell(withIdentifier: "test") as! MySecondCell
-        return cell
-    }
-    
-    
-}
