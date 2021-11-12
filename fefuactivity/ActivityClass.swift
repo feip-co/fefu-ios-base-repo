d3e2597f2f4c2c6fb8317688795200bee1061650
zahar <zahark13@gmail.com>
2021-11-13 01:18:24 +1000

    Table Ready

--- fefuactivity/ActivityClass.swift
+++ fefuactivity/ActivityClass.swift
@@ -14,25 +14,28 @@ class ActivityStartViewController:
         AlmostWorkingTableView.isHidden = false;
     }
     let idCell = "CellFinal"
-
+    let time = ["Вчера","Май 2022 года"]
     @IBOutlet weak var AlmostWorkingTableView: UITableView!
     @IBOutlet weak var FirstView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         AlmostWorkingTableView.isHidden = true;
         AlmostWorkingTableView.dataSource = self;
+        AlmostWorkingTableView.delegate = self;
         AlmostWorkingTableView.register(UINib(nibName: "CellFinal", bundle:nil), forCellReuseIdentifier: idCell)
+        self.AlmostWorkingTableView.rowHeight = 220
     }
 
 }
 
-extension ActivityStartViewController: UITableViewDataSource{
+extension ActivityStartViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellFinal", for: indexPath) as! CellFinal
+        cell.title.text = time[indexPath.row]
         return cell
     }
     
