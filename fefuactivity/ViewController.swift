86a002e22cdc0ea27fc297569fa3ba1ba2eba890
Tim <timatimof2002gmail.com>
2022-01-30 20:27:45 +1000

    First hw

---
+++ fefuactivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fefuactivity
+//
+//  Created by Andrew L on 04.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.backgroundColor = .red
+    }
+
+    
+    
+}
+
